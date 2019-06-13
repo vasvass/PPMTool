dd1c1af9be708499d3ccfe7b61d0c073b663949e
vasvass <vasvass@yahoo.com>
2019-06-13 16:56:21 +0300

    Find All Projects

--- PPMTool/src/main/java/com/vasvass/ppmtool/repositories/ProjectRepository.java
+++ PPMTool/src/main/java/com/vasvass/ppmtool/repositories/ProjectRepository.java
@@ -15,4 +15,7 @@ import org.springframework.stereotype.Repository;
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 
    Project findByProjectIdentifier(String projectId);
+
+   @Override
+   Iterable<Project> findAll();
 }
--- PPMTool/src/main/java/com/vasvass/ppmtool/web/ProjectController.java
+++ PPMTool/src/main/java/com/vasvass/ppmtool/web/ProjectController.java
@@ -45,5 +45,10 @@ public class ProjectController {
     return new ResponseEntity<Project>(project, HttpStatus.OK);
   }
 
+  @GetMapping("/all")
+  public Iterable<Project> getAllProjects(){
+    return projectService.findAllProjects();
+  }
+
 
 }
