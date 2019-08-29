ab55eed08d5780c3c8c554041ef7cdf43dd0a90f
vasvass <vasvass@yahoo.com>
2019-08-29 16:42:38 +0300

    Persist Project Task

---
+++ PPMTool/src/main/java/com/vasvass/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,52 @@
+package com.vasvass.ppmtool.services;
+
+import com.vasvass.ppmtool.domain.*;
+import com.vasvass.ppmtool.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p><i>Created on: 29/08/2019</i></p>
+ *
+ * @author vasvass
+ */
+
+@Service
+public class ProjectTaskService {
+
+  @Autowired
+  private BacklogRepository backlogRepository;
+
+  @Autowired
+  private ProjectTaskRepository projectTaskRepository;
+
+  public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
+
+    // All PTs to be added to a specific project, project != null, Backlog exists
+    Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+    // Set the Backlog to the ProjectTask
+    projectTask.setBacklog(backlog);
+    // Project sequence to be like this IDPRO-1 IDPRO-2 ...
+    Integer BacklogSequence = backlog.getPTSequence();
+    // Update the Backlog sequence
+    BacklogSequence++;
+
+    // add sequence to projectTask
+    projectTask.setProjectSequence(backlog.getProjectIdentifier()+"-"+BacklogSequence);
+    projectTask.setProjectIdentifier(projectIdentifier);
+
+    // INITIAL Priority when priority is null
+   // if(projectTask.getPriority()==0 || projectTask.getPriority()==null) {
+   //   projectTask.setPriority(3);
+   // }
+
+    // INITIAL status when status is null
+    if(projectTask.getStatus()=="" || projectTask.getStatus()==null){
+      projectTask.setStatus("TO_DO");
+    }
+
+ return projectTaskRepository.save(projectTask);
+  }
+
+
+}
---
+++ PPMTool/src/main/java/com/vasvass/ppmtool/web/BacklogController.java
@@ -0,0 +1,41 @@
+package com.vasvass.ppmtool.web;
+
+import com.vasvass.ppmtool.domain.ProjectTask;
+import com.vasvass.ppmtool.services.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * <p><i>Created on: 29/08/2019</i></p>
+ *
+ * @author vasvass
+ */
+
+@RestController
+@RequestMapping("/api/backlog")
+@CrossOrigin
+public class BacklogController {
+
+  @Autowired
+  private ProjectTaskService projectTaskService;
+
+  @Autowired
+  private MapValidationErrorService mapValidationErrorService;
+
+  @PostMapping("/{backlog_id}")
+  public ResponseEntity<?> addPTtoBacklog(@Valid @RequestBody ProjectTask projectTask, BindingResult result, @PathVariable String backlog_id) {
+
+     ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+     if (errorMap != null) return errorMap;
+
+     ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id, projectTask);
+
+     return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
+  }
+
+
+}
