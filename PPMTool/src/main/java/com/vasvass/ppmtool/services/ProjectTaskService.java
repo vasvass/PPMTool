3eb9846530dfcbfd2f7cc17e4911d672f4e2de9d
vasvass <vasvass@yahoo.com>
2019-11-01 16:16:59 +0200

    Fix bug with delete operation

--- PPMTool/src/main/java/com/vasvass/ppmtool/services/ProjectTaskService.java
+++ PPMTool/src/main/java/com/vasvass/ppmtool/services/ProjectTaskService.java
@@ -107,12 +107,7 @@ public class ProjectTaskService {
   public void deletePTByProjectSequence(String backlog_id, String pt_id){
     ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
 
-    Backlog backlog = projectTask.getBacklog();
-    List<ProjectTask> pts = backlog.getProjectTasks();
-    pts.remove(projectTask);
-    backlogRepository.save(backlog);
-
-    projectTaskRepository.delete(projectTask);
+     projectTaskRepository.delete(projectTask);
   }
    // Update project Task
 
